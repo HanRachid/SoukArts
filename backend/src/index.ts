48f27eb645edd2f826673e8cd3c2a08807cbc900
HanRachid <handaouirachid@gmail.com>
2023-10-25 15:04:23 +0100

    setup typescript and express in backend

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import  { NextFunction, Request, Response} from "express";
+import "./types"
+const express = require('express');
+const app = express();
+const port = process.env.PORT ?? 3000
+app.get('/',(req:Request,res:Response)=>{
+	console.log("deux");
+	
+	res.send('test');
+});
+
+app.listen(port,()=>{
+	console.log('server running on port '+port);
+	
+})
+
