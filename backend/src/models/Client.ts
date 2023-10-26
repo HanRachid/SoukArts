2d4d876af953740a7a36a9317d859918fe79c2f3
imanebk-github <imane.elbakir@gmail.com>
2023-10-26 12:33:49 +0100

    // Additional properties and methods specific to admin

--- backend/src/models/Client.ts
+++ backend/src/models/Client.ts
@@ -1,5 +1,7 @@
 import UserModel from "./UserModel";
-
+/**
+ *Client class for the client role
+ */
 export default class Client extends UserModel {
   // Additional properties and methods specific to client
 }
--- backend/src/models/UserModel.ts
+++ backend/src/models/UserModel.ts
@@ -1,10 +1,18 @@
 import BaseModel from "./BaseModel";
 
+/**
+ * UserModel Class that describes the users .
+ */
 export default class UserModel implements BaseModel {
   id: number;
   first_name: string;
   last_name: string;
-
+  /**
+   *
+   * @param id
+   * @param first_name
+   * @param last_name
+   */
   constructor(id: number, first_name: string, last_name: string) {
     this.id = id;
     this.first_name = first_name;
