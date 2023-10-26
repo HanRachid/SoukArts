35404f601b0b435f77ad5c2fd180198e198b0ab7
HanRachid <handaouirachid@gmail.com>
2023-10-26 08:41:03 +0100

    add test for middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+const middle = (req: Request, res: Response, next: NextFunction): void => {
+	console.log("middleware");
+
+	try {
+		next();
+	} catch {
+		console.error('An error occurred when calling next:');
+	}
+};
+
+app.use(middle);
 app.get('/', (req: Request, res: Response) => {
 	res.send('rachdzaid');
 });
 
-app.listen(3000, () => {
+app.listen(3000, (): void => {
 	console.log('server running on port ' + port);
 });
+
+export { middle };
