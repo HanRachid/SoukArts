701bd499e86d6bcc3b655f8c878aa8b76f32f74e
imanebk-github <imane.elbakir@gmail.com>
2023-10-26 12:33:49 +0100

    Add documentation to classes and interfaces

--- backend/src/models/Admin.ts
+++ backend/src/models/Admin.ts
@@ -1,5 +1,7 @@
 import UserModel from "./UserModel";
-
+/**
+ * Admin class for the Admin role
+ */
 export default class Admin extends UserModel {
   // Additional properties and methods specific to admin
 }
--- backend/src/models/BaseModel.ts
+++ backend/src/models/BaseModel.ts
@@ -1,8 +1,9 @@
-
-
+/**
+ * An interface describing a UserModel.
+ * @public
+ */
 export default interface BaseModel {
-	id: number;
-	first_name: string;
-	last_name: string;
-
+  id: number;
+  first_name: string;
+  last_name: string;
 }
