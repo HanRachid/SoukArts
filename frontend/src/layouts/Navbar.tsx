f7c5c08f9815cd9a96ad80f44de9a7280872213f
hanae <hanae.manekoudi@gmail.com>
2023-11-03 16:03:39 +0100

    create variable tailwind

--- frontend/src/layouts/Navbar.tsx
+++ frontend/src/layouts/Navbar.tsx
@@ -4,7 +4,7 @@ import favicon from '../assets/fav1.svg';
 export default function NavBar() {
   return (
     <>
-      <div className='flex justify-around items-center'>
+      <div className='flex justify-around items-center  bg'>
         <div>
           <label htmlFor='select'>All categories</label>
           <select name='' id=''></select>
