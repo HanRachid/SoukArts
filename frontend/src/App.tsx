32c7e240c8c0335b125c5fd90bc87aa6179f3ee9
hanae <hanae.manekoudi@gmail.com>
2023-11-03 15:25:35 +0100

    start css config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <>
-    
+    <h1 className = 'text-red-800'>COUCOU</h1>
     </>
   );
 }
 
 export default App;
+ 
