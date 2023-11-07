10a6d44c9e9390b7b36de7f4b4e9d615028cb9f4
hanae <hanae.manekoudi@gmail.com>
2023-11-07 13:34:07 +0100

    commit to pull structure projct

--- frontend/src/layouts/Footer.tsx
+++ frontend/src/layouts/Footer.tsx
@@ -1,3 +1,22 @@
+import logoFooter from './assets/monImage.jpg';
+
 export default function Footer() {
-  return <div> this is the Footer </div>;
+
+  return <>
+  <footer> 
+    <section className ='part-one-footer'>
+     <div>
+      <div>
+       <img src={logoFooter} alt="Description de l'image" />
+
+      </div>
+      <div></div>
+     </div>
+    </section>
+
+    <section className ='part-two-footer'>
+
+    </section>
+  </footer>
+    </>
 }
