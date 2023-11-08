6a862514a863cf882114db8b4c1781bf7effaa94
HanRachid <handaouirachid@gmail.com>
2023-11-08 12:50:53 +0100

    add navigation buttons

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -7,16 +7,19 @@ export default function Navigation() {
     <>
       <div className='flex items-center justify-between p-4 h-10 '>
         <Homenavigation />
+
         <Link to='/'>Become a Seller</Link>
-        <Link to='/'>
-          <img src={cart} className='w-6' alt='cart' />
-        </Link>
         <Link to='/'>
           <img src={heart} className='w-6' alt='heart' />
         </Link>
-
-        <Link to='/'>Login</Link>
-        <Link to='/'>Join In</Link>
+        <Link to='/'>
+          <img src={cart} className='w-6' alt='cart' />
+        </Link>
+        <div className='flex gap-3'>
+          <Link to='/'>Login</Link>
+          <span>|</span>
+          <Link to='/'>Join In</Link>
+        </div>
       </div>
     </>
   );
