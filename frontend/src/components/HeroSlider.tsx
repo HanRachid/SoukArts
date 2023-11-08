cf91524fec50e493c7ae46d07b2d984ba0de5961
imanebk-github <imane.elbakir@gmail.com>
2023-11-08 18:47:09 +0100

    Add Hero image slider component

---
+++ frontend/src/components/HeroSlider.tsx
@@ -0,0 +1,64 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+
+import "./css/HeroSlider.css";
+
+// import required modules
+import { Autoplay, Pagination, EffectFade } from "swiper/modules";
+
+export default function HeroSlider({
+  image1,
+  image2,
+  image3,
+  image4,
+  children,
+}: {
+  image1: string;
+  image2: string;
+  image3: string;
+  image4: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col items-center h-screen relative mt-[160px]">
+      <h1 className="font-primary text-headingXl absolute top-[-80px] z-10 tracking-wide">
+        {children}
+      </h1>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        effect={"fade"}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        //navigation={true}
+        modules={[Autoplay, Pagination, EffectFade]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={image1} alt="image1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <img src={image2} alt="image2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <img src={image3} alt="image3" />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <img src={image4} alt="image4" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
