62165a390ea870e4d0a4645b8605310e80b2be93
HanRachid <handaouirachid@gmail.com>
2023-11-13 10:41:27 +0100

    add headlessui to package json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,7 @@
 import {NextFunction, Request, Response} from 'express';
 import {connectDb} from './config/connectionDb';
 import UserModel from './models/UserModel';
-import {getUsers, cleanUsers, registerUser} from './routes/routeUser';
-import {BSON, ObjectId} from 'mongodb';
+import {getUsers, cleanUsers, registerUser} from './routes/UserRoutes';
 
 const express = require('express');
 
