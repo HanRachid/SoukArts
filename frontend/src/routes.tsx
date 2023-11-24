c5c26ff41a1c06cefb3d386f0d3a3c86a5e3e0ef
HanRachid <handaouirachid@gmail.com>
2023-11-24 10:27:17 +0100

    remove daisyui and fix modules

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,45 +1,44 @@
-import { RouteObj } from "../types";
-import DashBoard from "./pages/Dashboard";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Privacy from "./pages/Privacy";
-import Register from "./pages/Register";
-import Termsofuse from "./pages/TermsOfUse";
-import GlobalLayout from "./layouts/GlobalLayout";
-// import RugProducts from "./pages/RugProducts";
-import ProductsList from "./pages/Category-Product";
-import Logout from "./pages/Logout";
+import {RouteObj} from '../types';
+import DashBoard from './pages/Dashboard';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Privacy from './pages/Privacy';
+import Register from './pages/Register';
+import Termsofuse from './pages/TermsOfUse';
+import GlobalLayout from './layouts/GlobalLayout';
+import ProductsList from './pages/ProductsList';
+import Logout from './pages/Logout';
 export default [
   {
-    path: "/",
+    path: '/',
     element: <GlobalLayout Component={Home} hasLayout={true} />,
   },
   {
-    path: "/login",
+    path: '/login',
     element: <GlobalLayout Component={Login} hasLayout={false} />,
   },
   {
-    path: "/register",
+    path: '/register',
     element: <GlobalLayout Component={Register} hasLayout={false} />,
   },
   {
-    path: "/dashboard",
+    path: '/dashboard',
     element: <GlobalLayout Component={DashBoard} hasLayout={false} />,
   },
   {
-    path: "/termsofuse",
+    path: '/termsofuse',
     element: <GlobalLayout Component={Termsofuse} hasLayout={false} />,
   },
   {
-    path: "/privacy",
+    path: '/privacy',
     element: <GlobalLayout Component={Privacy} hasLayout={false} />,
   },
   {
-    path: "/categories/:category",
+    path: '/categories/:category',
     element: <GlobalLayout Component={ProductsList} hasLayout={true} />,
   },
   {
-    path: "/logout",
+    path: '/logout',
     element: <GlobalLayout Component={Logout} hasLayout={true} />,
   },
 ] as RouteObj[];
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,38 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui";
-import withMT from "@material-tailwind/react/utils/withMT";
+import withMT from '@material-tailwind/react/utils/withMT';
 export default withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        colorBlack: "#292727",
-        colorBrown: "#3F382B",
+        colorBlack: '#292727',
+        colorBrown: '#3F382B',
         black: {
-          50: "#f6f5f5",
-          100: "#e7e6e6",
-          200: "#d2cfd0",
-          300: "#b2aeaf",
-          400: "#8b8586",
-          500: "#706a6b",
-          600: "#605a5b",
-          700: "#514d4d",
-          800: "#474343",
-          900: "#3e3b3b",
-          950: "#292727",
+          50: '#f6f5f5',
+          100: '#e7e6e6',
+          200: '#d2cfd0',
+          300: '#b2aeaf',
+          400: '#8b8586',
+          500: '#706a6b',
+          600: '#605a5b',
+          700: '#514d4d',
+          800: '#474343',
+          900: '#3e3b3b',
+          950: '#292727',
         },
-        colorGold: "#B48F57",
-        colorBeige: "#D8CBC4",
-        colorBeigeLight: "#EBE6E4",
-        colorLight: "#EBE6E3",
+        colorGold: '#B48F57',
+        colorBeige: '#D8CBC4',
+        colorBeigeLight: '#EBE6E4',
+        colorLight: '#EBE6E3',
       },
       fontSize: {
-        small: "1rem",
-        medium: "1.125rem",
-        large: "1.5rem",
-        extraLarge: "2.5rem",
-        heading: "3.375rem",
-        headingXl: "4.0625rem",
+        small: '1rem',
+        medium: '1.125rem',
+        large: '1.5rem',
+        extraLarge: '2.5rem',
+        heading: '3.375rem',
+        headingXl: '4.0625rem',
       },
       fontFamily: {
         primary: ['BrownSugar', 'sans-serif'],
@@ -40,5 +39,4 @@ export default withMT({
       },
     },
   },
-  plugins: [daisyui],
 });
