317ecb3e4fac95a88b0f17dd2f3bff55e5abb8d7
imanebk-github <imane.elbakir@gmail.com>
2023-11-29 17:28:04 +0100

    create route for page store

---
+++ frontend/src/pages/StoreSeller.tsx
@@ -0,0 +1,9 @@
+export default function StoreSeller() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,161 +1,166 @@
-import {RouteObj} from '../types';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Privacy from './pages/Privacy';
-import Register from './pages/Register';
-import Termsofuse from './pages/TermsOfUse';
-import GlobalLayout from './layouts/GlobalLayout';
-import ProductDetail from './pages/ProductDetail';
-import SearchResult from './pages/SearchResults';
-import ProductsList from './pages/ProductsList';
-import Logout from './pages/Logout';
-import Payment from './pages/Payment';
-import DeliveryPayment from './pages/DeliveryPayment';
-import ReviewPayment from './pages/ReviewPayment';
-import PaymentLayout from './layouts/PaymentLayout';
-import Favorite from './pages/Favorite';
-import DashboardLayout from './layouts/DashboardLayout';
-import Dashboard from './views/Dashboard';
-import Products from './views/Products';
-import AddNewProduct from './components/products/AddNewProduct';
-import SellerLinks from './DashboardLinks/SellerLinks';
-import Cart from './pages/Cart';
+import { RouteObj } from "../types";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Privacy from "./pages/Privacy";
+import Register from "./pages/Register";
+import Termsofuse from "./pages/TermsOfUse";
+import GlobalLayout from "./layouts/GlobalLayout";
+import ProductDetail from "./pages/ProductDetail";
+import SearchResult from "./pages/SearchResults";
+import ProductsList from "./pages/ProductsList";
+import Logout from "./pages/Logout";
+import Payment from "./pages/Payment";
+import DeliveryPayment from "./pages/DeliveryPayment";
+import ReviewPayment from "./pages/ReviewPayment";
+import PaymentLayout from "./layouts/PaymentLayout";
+import Favorite from "./pages/Favorite";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Dashboard from "./views/Dashboard";
+import Products from "./views/Products";
+import AddNewProduct from "./components/products/AddNewProduct";
+import SellerLinks from "./DashboardLinks/SellerLinks";
+import Cart from "./pages/Cart";
+import StoreSeller from "./pages/StoreSeller";
 export default [
   {
-    path: '/',
+    path: "/",
     element: <GlobalLayout Component={Home} hasLayout={true} />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <GlobalLayout Component={Login} hasLayout={false} />,
   },
   {
-    path: '/register',
+    path: "/register",
     element: <GlobalLayout Component={Register} hasLayout={false} />,
   },
 
   {
-    path: '/termsofuse',
+    path: "/termsofuse",
     element: <GlobalLayout Component={Termsofuse} hasLayout={false} />,
   },
   {
-    path: '/privacy',
+    path: "/privacy",
     element: <GlobalLayout Component={Privacy} hasLayout={false} />,
   },
   {
-    path: '/categories/:category',
+    path: "/categories/:category",
     element: <GlobalLayout Component={ProductsList} hasLayout={true} />,
   },
   {
-    path: '/deliverypayment',
+    path: "/deliverypayment",
     element: <PaymentLayout Component={DeliveryPayment} activeStep={0} />,
   },
   {
-    path: '/payment',
+    path: "/payment",
     element: <PaymentLayout Component={Payment} activeStep={1} />,
   },
   {
-    path: '/reviewpayment',
+    path: "/reviewpayment",
     element: <PaymentLayout Component={ReviewPayment} activeStep={2} />,
   },
   {
-    path: '/products/:product',
+    path: "/products/:product",
     element: <GlobalLayout Component={ProductDetail} hasLayout={true} />,
   },
   {
-    path: '/logout',
+    path: "/logout",
     element: <GlobalLayout Component={Logout} hasLayout={true} />,
   },
   {
-    path: '/search/:category/:terms',
+    path: "/search/:category/:terms",
     element: <GlobalLayout Component={SearchResult} hasLayout={true} />,
   },
   {
-    path: '/favorite',
+    path: "/favorite",
     element: <GlobalLayout Component={Favorite} hasLayout={true} />,
   },
   {
-    path: '/Cart',
+    path: "/Cart",
     element: <GlobalLayout Component={Cart} hasLayout={true} />,
   },
   {
-    path: '/dashboard',
+    path: "/dashboard",
     element: (
       <DashboardLayout
-        pageTitle={'Dashboard'}
+        pageTitle={"Dashboard"}
         Component={Dashboard}
         links={SellerLinks}
       />
     ),
   },
   {
-    path: '/Dashboard/Products',
+    path: "/Dashboard/Products",
     element: (
       <DashboardLayout
-        pageTitle={'Dashboard/Products'}
+        pageTitle={"Dashboard/Products"}
         Component={Products}
         links={SellerLinks}
       />
     ),
   },
   {
-    path: '/Dashboard/Orders',
+    path: "/Dashboard/Orders",
     element: (
       <DashboardLayout
-        pageTitle={'/Dashboard/Orders'}
+        pageTitle={"/Dashboard/Orders"}
         Component={Products}
         links={SellerLinks}
       />
     ),
   },
   {
-    path: '/Dashboard/Stats',
+    path: "/Dashboard/Stats",
     element: (
       <DashboardLayout
-        pageTitle={'/Dashboard/Stats'}
+        pageTitle={"/Dashboard/Stats"}
         Component={Products}
         links={SellerLinks}
       />
     ),
   },
   {
-    path: '/Dashboard/Finance',
+    path: "/Dashboard/Finance",
     element: (
       <DashboardLayout
-        pageTitle={'/Dashboard/Finance'}
+        pageTitle={"/Dashboard/Finance"}
         Component={Products}
         links={SellerLinks}
       />
     ),
   },
   {
-    path: '/Dashboard/Settings',
+    path: "/Dashboard/Settings",
     element: (
       <DashboardLayout
-        pageTitle={'/Dashboard/Settings'}
+        pageTitle={"/Dashboard/Settings"}
         Component={Products}
         links={SellerLinks}
       />
     ),
   },
   {
-    path: '/Dashboard/Marketing',
+    path: "/Dashboard/Marketing",
     element: (
       <DashboardLayout
-        pageTitle={'/Dashboard/Marketing'}
+        pageTitle={"/Dashboard/Marketing"}
         Component={Products}
         links={SellerLinks}
       />
     ),
   },
   {
-    path: 'Dashboard/products/addNewProduct',
+    path: "Dashboard/products/addNewProduct",
     element: (
       <DashboardLayout
-        pageTitle={'Dashboard/Products'}
+        pageTitle={"Dashboard/Products"}
         Component={AddNewProduct}
         links={SellerLinks}
       />
     ),
+  },
+  {
+    path: "/Store",
+    element: <GlobalLayout Component={StoreSeller} hasLayout={true} />,
   },
 ] as RouteObj[];
