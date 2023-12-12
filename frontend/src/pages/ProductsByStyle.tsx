a13f037bfb8e978501c218ee4fac93831ddb8f85
imanebk-github <imane.elbakir@gmail.com>
2023-12-12 13:18:58 +0100

    shop by style page

--- frontend/src/pages/ProductsByStyle.tsx
+++ frontend/src/pages/ProductsByStyle.tsx
@@ -6,6 +6,9 @@ import classicPic from "../assets/home/ShopByStyle/classic.webp";
 import modernPic from "../assets/home/ShopByStyle/Modern.jpg";
 import bohoPic from "../assets/home/ShopByStyle/Boho.jpg";
 import fusionPic from "../assets/home/ShopByStyle/Fusion.jpg";
+import { Pagination } from "antd";
+import ProductCard from "../components/categoryproducts/ProductCard";
+import SelectOptions from "../components/categoryproducts/Select";
 
 const styles: {
   name: string;
@@ -40,8 +43,8 @@ export default function ProductsByyStyle() {
   );
   return (
     <>
-      <div className="ml-[244px] mr-[244px] mt-8 ">
-        <div className="flex justify-center items-center gap-8">
+      <div className="ml-[244px] mr-[244px] mt-16 ">
+        <div className="flex flex-col justify-center items-center gap-8">
           <div className="w-[850px] h-[600px] rounded-tl-full rounded-tr-full overflow-hidden outline-offset-8 outline">
             <img
               src={selectedStyle.image}
@@ -49,61 +52,34 @@ export default function ProductsByyStyle() {
               className="w-full h-full object-cover"
             />
           </div>
-          <div className="w-[850px]">
-            <h1 className="text-left tracking-widest leading-normal text-9xl font-primary text-colorBlack">
+          <div>
+            <h1 className="text-center tracking-widest leading-normal text-8xl font-primary text-colorBlack">
               {selectedStyle.name?.toUpperCase()} STYLE
             </h1>
           </div>
         </div>
-        {/* <div className="mt-8 border-b border-gray-500 my-4"></div>
-        <div className="flex justify-around mt-16">
-          {subcategories?.map((subcategory) => (
-            <div className="w-32 flex flex-col justify-start items-center">
-              <div className="h-32 w-32 rounded-full border-2 overflow-hidden hover:scale-[1.06] hover:-outline-offset-2 outline-2 outline outline-colorBlack ">
-                <img
-                  src={subcategory?.image}
-                  alt=""
-                  className="w-full h-full object-cover"
-                />
-              </div>
-              <p className="mt-3 font-secondary text-center text-lg text-colorBlack">
-                {subcategory?.name}
-              </p>
-            </div>
-          ))}
-        </div>
-        <div className=" h-auto flex justify-between items-center mt-14 z-50">
-          <div className="flex items-center gap-5">
-            <Filter />
-            <div>
-              <span className="font-secondary font-medium">18019 products</span>
-            </div>
-          </div>
+        <div className="mt-8 border-b border-gray-500 my-4"></div>
+        <div className="flex justify-end">
           <SelectOptions
             selections={[
-              "Popular",
-              "Lowest price",
-              "Highest price",
-              "Top customer reviews",
-              "Most Recent",
+              "Rugs",
+              "Poufs",
+              "Lamps",
+              "Pillows",
+              "Shoes",
+              "bags",
+              "jewelry",
             ]}
           />
         </div>
         <div className="mt-11 grid grid-cols-3 gap-6">
-          {/* <ProductCard isNew={true} image={rug6} /> */}
-        {/* </div>
+          {productsStyle.map((product, index) => (
+            <ProductCard key={index} isNew={true} product={product} />
+          ))}
+        </div>
         <div className="mt-9 flex justify-center">
           <Pagination count={10} variant="outlined" shape="rounded" />
         </div>
-        <div>
-          <h2 className="font-secondary text-[1.5rem]">RECENTLY VIEWED</h2>
-          <div className="mt-3 grid grid-cols-4 gap-[3.3rem]">
-            <RecentCard isNew={true} image={rug7} />
-            <RecentCard isNew={true} image={Pouf1} />
-            <RecentCard isNew={true} image={jewelry3} />
-            <RecentCard isNew={true} image={lamp1} />
-          </div>
-        </div> */}
       </div>
     </>
   );
